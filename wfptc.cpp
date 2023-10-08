f7bf57eb58a634e2b9fa7f852252ad9aa28f7e8f
Dakota Hack <103958555+dhack01@users.noreply.github.com>
2023-10-08 19:40:08 -0400

    Add files via upload

---
+++ wfptc.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include "StackType.h"
+
+using namespace std;
+
+int WFPTagCounts(string inp) {
+	int counter = 0;
+	StackType<char> stack(100);
+	char temp = 'a';
+	for (char ch : inp) {
+		if (ch == '<' || ch == '(' || ch == '[') {
+			stack.Push(ch);
+		}
+		else if (stack.IsEmpty() != 1 && ch == ']' || stack.IsEmpty() != 1 && ch == ')' || stack.IsEmpty() != 1 && ch == '>') {
+			temp = stack.Pop();
+			if (ch == ')' && temp != '(' || ch == ']' && temp != '[' || ch == '>' && temp != '<') {
+				cout << ch;
+				cout << temp;
+				return -1;
+			}
+			else if (ch == '>' && temp == '<') {
+				counter++;
+			}
+
+		}
+	}
+	if (stack.IsEmpty() == 1) {
+		return counter;
+	}
+	return -1;
+	
+}
+/*Used bellow code to test
+int main() {
+	string inp;
+	cout << "Please enter test case: ";
+	cin >> inp;
+	cout << WFPTagCounts(inp);
+	return 0;
+}
+*/
